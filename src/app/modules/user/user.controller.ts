fd01880ec65791f7b654df44539fae63dcb9151d
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-10-01 00:42:14 +0600

    create a user successful

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,16 +2,17 @@ import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
 import { responseForData } from '../../../shared/sendResponse';
 import { Request, Response } from 'express';
+import { userService } from './user.service';
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const user = req.body;
 
-  // const result = await userService.createStudent(user);
+  const result = await userService.createUser(user);
   responseForData.sendResponseForCreate(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Student created Successful',
-    // data: result,
+    data: result,
   });
 });
 
