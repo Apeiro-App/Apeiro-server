a77a77cb8fc880d256615f258a4fd3c40327b4d9
siamalrafi <rahathasandipto@gmail.com>
2023-10-03 00:03:43 +0600

    [DONE] coach model and interface

--- src/app/modules/coach/coach.model.ts
+++ src/app/modules/coach/coach.model.ts
@@ -1,1 +1,55 @@
-coach;
+import { Schema, model } from 'mongoose';
+import { ICoachInfo } from './coach.interface';
+
+const coachSchema = new Schema<ICoachInfo>(
+  {
+    name: {
+      coachFirstName: {
+        type: String,
+        required: true,
+      },
+      coachLastName: {
+        type: String,
+        required: true,
+      },
+    },
+    adminId: {
+      type: String,
+      required: true,
+    },
+    coachId: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      required: true,
+    },
+    number: {
+      type: Number,
+      required: true,
+    },
+    birthDate: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female'],
+      required: true,
+    },
+    owner: {
+      type: String,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Create the Coach model
+const Coach = model<ICoachInfo>('Coach', coachSchema);
