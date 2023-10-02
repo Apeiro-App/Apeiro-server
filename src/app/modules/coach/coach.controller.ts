ff94bddfbcde91958d81d3d3a19120e384949f7b
siamalrafi <rahathasandipto@gmail.com>
2023-10-03 00:24:25 +0600

    [DONE] coach model, rotue and validaton

--- src/app/modules/coach/coach.controller.ts
+++ src/app/modules/coach/coach.controller.ts
@@ -1,1 +1,11 @@
-coach;
+import { Request, Response } from 'express';
+import { RequestHandler } from 'express-serve-static-core';
+import catchAsync from '../../../shared/catchAsync';
+
+const createCoach: RequestHandler = catchAsync(
+  async (req: Request, res: Response) => {},
+);
+
+export const CoachController = {
+  createCoach: createCoach,
+};
--- src/app/modules/coach/coach.validation.ts
+++ src/app/modules/coach/coach.validation.ts
@@ -1,1 +1,38 @@
-coach;
+import { z } from 'zod';
+
+const coachSchema = z.object({
+  name: z.object({
+    coachFirstName: z.string({
+      required_error: 'Coach first name is required',
+    }),
+    coachLastName: z.string({
+      required_error: 'Coach last name is required',
+    }),
+  }),
+  adminId: z.string({
+    required_error: 'Admin ID is required',
+  }),
+  coachId: z.string({
+    required_error: 'Coach ID is required',
+  }),
+  isVerified: z.boolean({
+    required_error: 'Verification status is required',
+  }),
+  number: z.number({
+    required_error: 'Number is required',
+  }),
+  birthDate: z.string({
+    required_error: 'Birth date is required',
+  }),
+  gender: z.enum(['male', 'female'], {
+    required_error: 'Gender is required',
+  }),
+  owner: z.string().optional(),
+  role: z.string({
+    required_error: 'Role is required',
+  }),
+});
+
+export const CoachValidation = {
+  coachSchema,
+};
