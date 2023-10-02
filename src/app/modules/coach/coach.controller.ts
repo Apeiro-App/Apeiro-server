045d644cf28f1399525680136518c90ecd706ff4
siamalrafi <rahathasandipto@gmail.com>
2023-10-03 00:29:58 +0600

    [DONE] coach model, rotue and validaton

--- src/app/modules/coach/coach.controller.ts
+++ src/app/modules/coach/coach.controller.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from 'express';
 import { RequestHandler } from 'express-serve-static-core';
 import catchAsync from '../../../shared/catchAsync';
+import { UserService } from './coach.services';
+import httpStatus from 'http-status';
+import { ICoachInfo } from './coach.interface';
+import sendResponse from '../../../shared/sendResponse';
 
 const createCoach: RequestHandler = catchAsync(
-  async (req: Request, res: Response) => {},
+  async (req: Request, res: Response) => {
+    const { student, ...userData } = req.body;
+
+    const result = await UserService.createStudent(student, userData);
+
+    sendResponse<ICoachInfo>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'user created successfully!',
+      data: result,
+    });
+  },
 );
 
 export const CoachController = {
-  createCoach: createCoach,
+  createCoach,
 };
