c7a4563828e92304f2e39c797923322a01c0a7af
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-10-03 13:22:49 +0600

    set refrest token at cokkie browser

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,18 +4,30 @@ import { responseForData } from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
 import { authService } from './auth.service';
 import { ILoginUserResponse } from './auth.interface';
+import config from '../../../config';
 
 // login a user
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body;
-  //   console.log(loginData);
   const result = await authService.loginUser(loginData);
+  const { refreshToken, ...others } = result;
+
+  //   set refresh token at browser cookie
+  const cookieOption = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  };
+  res.cookie('refreshToken', refreshToken, cookieOption);
+
+  if ('refreshToken' in result) {
+    delete result.refreshToken;
+  }
 
   responseForData.sendResponseForCreate<ILoginUserResponse>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'login Successful',
-    data: result,
+    data: others,
   });
   // next();
 });
