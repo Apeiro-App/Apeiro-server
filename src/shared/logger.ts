40c268a93d75b8df4c6155838d06d8f7fdbc2cd8
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-10-16 21:53:34 +0600

    remove logger function

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,67 +1,67 @@
-import { createLogger, format, transports } from 'winston';
-import path from 'path';
-const { combine, timestamp, label, printf, prettyPrint } = format;
-import DailyRotateFile from 'winston-daily-rotate-file';
+// import { createLogger, format, transports } from 'winston';
+// import path from 'path';
+// const { combine, timestamp, label, printf, prettyPrint } = format;
+// import DailyRotateFile from 'winston-daily-rotate-file';
 
-// custom log formate
-const myFormat = printf(({ level, message, label, timestamp }) => {
-  const date = new Date(timestamp);
-  const hour = date.getHours();
-  const minute = date.getMinutes();
-  const second = date.getSeconds();
-  return `${date.toDateString()}:${hour}: ${minute}:${second} [${label}] ${level}: ${message}`;
-});
+// // custom log formate
+// const myFormat = printf(({ level, message, label, timestamp }) => {
+//   const date = new Date(timestamp);
+//   const hour = date.getHours();
+//   const minute = date.getMinutes();
+//   const second = date.getSeconds();
+//   return `${date.toDateString()}:${hour}: ${minute}:${second} [${label}] ${level}: ${message}`;
+// });
 
-const logger = createLogger({
-  level: 'info',
-  format: combine(
-    label({ label: 'Gym Management' }),
-    timestamp(),
-    myFormat,
-    prettyPrint(),
-  ),
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-      filename: path.join(
-        process.cwd(),
-        'logs',
-        'winston',
-        'successes',
-        'Gym-%DATE%-success.log',
-      ),
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-  ],
-});
+// const logger = createLogger({
+//   level: 'info',
+//   format: combine(
+//     label({ label: 'Gym Management' }),
+//     timestamp(),
+//     myFormat,
+//     prettyPrint(),
+//   ),
+//   transports: [
+//     new transports.Console(),
+//     new DailyRotateFile({
+//       filename: path.join(
+//         process.cwd(),
+//         'logs',
+//         'winston',
+//         'successes',
+//         'Gym-%DATE%-success.log',
+//       ),
+//       datePattern: 'YYYY-MM-DD-HH',
+//       zippedArchive: true,
+//       maxSize: '20m',
+//       maxFiles: '14d',
+//     }),
+//   ],
+// });
 
-const errorLogger = createLogger({
-  level: 'error',
-  format: combine(
-    label({ label: 'Gym Management' }),
-    timestamp(),
-    myFormat,
-    prettyPrint(),
-  ),
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-      filename: path.join(
-        process.cwd(),
-        'logs',
-        'winston',
-        'errors',
-        'Gym-%DATE%-error.log',
-      ),
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-  ],
-});
+// const errorLogger = createLogger({
+//   level: 'error',
+//   format: combine(
+//     label({ label: 'Gym Management' }),
+//     timestamp(),
+//     myFormat,
+//     prettyPrint(),
+//   ),
+//   transports: [
+//     new transports.Console(),
+//     new DailyRotateFile({
+//       filename: path.join(
+//         process.cwd(),
+//         'logs',
+//         'winston',
+//         'errors',
+//         'Gym-%DATE%-error.log',
+//       ),
+//       datePattern: 'YYYY-MM-DD-HH',
+//       zippedArchive: true,
+//       maxSize: '20m',
+//       maxFiles: '14d',
+//     }),
+//   ],
+// });
 
-// export { errorLogger, logger };
+// // export { errorLogger, logger };
